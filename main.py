e4109d3aa73b4cbc9d3d565ebf0bbd3f4fa188d5
Daniel Clarke <u8sand@gmail.com>
2016-06-07 14:33:21 -0400

    Custom decorator for create WordCloud
    - Added some starter code for genes and free_text
    - Created templates for everything else
    
    Daniel Clarke

--- main.py
+++ main.py
@@ -1,42 +1,52 @@
 #!/usr/local/bin/python3
 
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 app = Flask(__name__, static_folder='static')
 
 @app.route('/')
 def index():
 	return render_template('index.html')
-	
-@app.route('/create/genes')
-def create_genes():
-	return render_template('create/genes.html')
-
-@app.route('/create/free_text')
-def create_free_text():
-	return render_template('create/free_text.html')
-
-@app.route('/create/url')
-def create_url():
-	return render_template('create/url.html')
-
-@app.route('/create/author')
-def create_author():
-	return render_template('create/author.html')
-
-@app.route('/create/pubmed')
-def create_pubmed():
-	return render_template('create/pubmed.html')
-
-@app.route('/create/bmc')
-def create_bmc():
-	return render_template('create/bmc.html')
-
-@app.route('/view')
-def view():
-	return render_template('view.html')
 
 @app.route('/help')
 def help():
 	return render_template('help.html')
+
+
+def route_page(func):
+	name = func.__name__
+	def create():
+		return render_template('create/%s.html' % (name))
+	app.add_url_rule('/create/%s' % (name), 'create_%s' % (name), create)
+	app.add_url_rule('/view/%s' % (name), 'view_%s' % (name), func, methods=['POST'])
+
+@route_page
+def genes():
+	return render_template('view/genes.html',
+		genes=request.form['genes'],
+		source=request.form['source'])
+
+@route_page
+def free_text():
+	return render_template('view/free_text.html',
+		text=request.form['text'],
+		stopwords=request.form['stopwords'],
+		biostopwords=request.form['biostopwords'])
+
+@route_page
+def url():
+	return render_template('view/url.html')
+
+@route_page
+def author():
+	return render_template('view/author.html')
+
+@route_page
+def pubmed():
+	return render_template('view/pubmed.html')
+
+@route_page
+def bmc():
+	return render_template('view/bmc.html')
+
 
 app.run(debug=True)
