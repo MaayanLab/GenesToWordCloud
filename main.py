b94687a4454a4bc4d4d044795476c40b0b0a4c2b
Daniel Clarke <u8sand@gmail.com>
2016-06-07 12:36:36 -0400

    Grew site structure to match old site
    - Brought in old site's Help page and related images
    - Added sub-menus for Create and Help
    
    Daniel Clarke

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#!/bin/python3
+#!/usr/local/bin/python3
 
 from flask import Flask, render_template, url_for
 app = Flask(__name__, static_folder='static')
@@ -6,10 +6,30 @@ app = Flask(__name__, static_folder='static')
 @app.route('/')
 def index():
 	return render_template('index.html')
+	
+@app.route('/create/genes')
+def create_genes():
+	return render_template('create/genes.html')
 
-@app.route('/create')
-def create():
-	return render_template('create.html')
+@app.route('/create/free_text')
+def create_free_text():
+	return render_template('create/free_text.html')
+
+@app.route('/create/url')
+def create_url():
+	return render_template('create/url.html')
+
+@app.route('/create/author')
+def create_author():
+	return render_template('create/author.html')
+
+@app.route('/create/pubmed')
+def create_pubmed():
+	return render_template('create/pubmed.html')
+
+@app.route('/create/bmc')
+def create_bmc():
+	return render_template('create/bmc.html')
 
 @app.route('/view')
 def view():
